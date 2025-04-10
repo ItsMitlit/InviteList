89f265cad77445bd83853216ba97efa98b018d11
Mitlit <64767472+ItsMitlit@users.noreply.github.com>
2025-04-11 09:14:20 +1200

    Update InviteListExecutor.java

--- src/main/java/it/mitl/inviteList/CommandExecutors/InviteListExecutor.java
+++ src/main/java/it/mitl/inviteList/CommandExecutors/InviteListExecutor.java
@@ -1,4 +1,4 @@
-package it.mitl.invlteList.CommandExecutors;
+package it.mitl.inviteList.CommandExecutors;
 
 import org.bukkit.ChatColor;
 import org.bukkit.command.Command;
@@ -6,15 +6,24 @@ import org.bukkit.command.CommandExecutor;
 import org.bukkit.command.CommandSender;
 
 public class InviteListExecutor implements CommandExecutor {
+    public InviteListExecutor() {
+    }
 
-    @Override
     public boolean onCommand(CommandSender sender, Command command, String label, String[] args) {
         if (args.length == 0) {
-            sender.sendMessage(ChatColor.RED + "Invalid usage. Use /invitelist help for help.");
+            sender.sendMessage(String.valueOf(ChatColor.RED) + "Invalid usage. Use /invitelist help for help.");
+            return true;
+        } else if (args[0].equalsIgnoreCase("help")) {
+            sender.sendMessage(String.valueOf(ChatColor.GOLD) + "InviteList Help");
+            sender.sendMessage(String.valueOf(ChatColor.GOLD) + "/invite <player> - Adds a player to the whitelist.");
+            sender.sendMessage(String.valueOf(ChatColor.GOLD) + "/invitelist help - Displays this help message.");
+            sender.sendMessage(String.valueOf(ChatColor.GOLD) + "/invitelist site - Responds with a link to the modrinth page.");
+            return true;
+        } else if (args[0].equalsIgnoreCase("site")) {
+            sender.sendMessage(String.valueOf(ChatColor.GOLD) + "Modrinth -> https://modrinth.com/plugin/invitelist)");
             return true;
         } else {
             return false;
         }
     }
-
 }
