caefadeb7b3e57ad31d15ebe2a2494ae68bb707c
Mitlit <64767472+ItsMitlit@users.noreply.github.com>
2025-04-11 09:13:16 +1200

    Update PlayerLoginListener.java

--- src/main/java/it/mitl/inviteList/Listeners/PlayerLoginListener.java
+++ src/main/java/it/mitl/inviteList/Listeners/PlayerLoginListener.java
@@ -1,17 +1,25 @@
 package it.mitl.inviteList.Listeners;
 
+import it.mitl.inviteList.Subroutines.WhitelistManager;
+import java.util.UUID;
 import org.bukkit.event.EventHandler;
 import org.bukkit.event.Listener;
-import org.bukkit.event.player.PlayerJoinEvent;
+import org.bukkit.event.player.PlayerLoginEvent;
+import org.bukkit.event.player.PlayerLoginEvent.Result;
 
-public class PlayerJoinListener implements Listener {
+public class PlayerLoginListener implements Listener {
+    public PlayerLoginListener() {
+    }
 
     @EventHandler
-    public void onPlayerJoin(PlayerJoinEvent event) {
-        // Code here
-
+    public void onPlayerLogin(PlayerLoginEvent event) {
+        UUID playerUUID = event.getPlayer().getUniqueId();
         if (event.getPlayer().isOp()) {
-            event.
+            event.allow();
+        } else if (WhitelistManager.isWhitelisted(playerUUID) == 0) {
+            event.allow();
+        } else {
+            event.disallow(Result.KICK_WHITELIST, "You are not whitelisted on this server. Ask someone to invite you with /invite <player> or do it through console.");
         }
 
     }
